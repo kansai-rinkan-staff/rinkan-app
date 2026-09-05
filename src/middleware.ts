8d78d07cd8d481a865ba637a0795ba6aa9720931
Kansai Rinkan Staff <kansairinkan.comm1ttee@gmail.com>
2026-09-05 22:57:50 +0900

    Add Basic Auth middleware

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+﻿import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization');
+  
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
+
+    if (user === 'rinkan' && pwd === 'kansai2026') {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse('Unauthorized.', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  });
+}
+
+export const config = {
+  matcher: '/:path*',
+};
